ebb5036ef52b47ca9b7a8e22d454197f04f5444b
Cheenou Xiong <cheenou.xiong@fewidev.com>
2022-01-25 10:55:40 -0600

    change importedModels diretory location

--- src/LoadSrc/srcFiles.js
+++ src/LoadSrc/srcFiles.js
@@ -1,12 +1,12 @@
 
 export function oilTankURL(){
-    return 'src/importedModels/old_oil_tank_.2/scene.gltf'
+    return '/importedModels/old_oil_tank_.2/scene.gltf'
 }
 
 export function noColorMachine(){
-    return 'src/importedModels/factory_machine_noColor/scene.gltf'
+    return 'importedModels/factory_machine_noColor/scene.gltf'
 }
 
 export function talorMachine(){
-    return 'src/importedModels/factory_machine/scene.gltf'
+    return 'importedModels/factory_machine/scene.gltf'
 }
