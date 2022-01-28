27ea1362d7c39ab63fa11dae7cc0115889df151c
Cheenou Xiong <cheenou.xiong@fewidev.com>
2022-01-28 16:17:43 -0600

    re run deployment
    Former-commit-id: 75ef0ea4afb8d9ae1fef1fa44fa9d1bd2045e6c9

--- src/PlatForms/systemControls/Renderer.js
+++ src/PlatForms/systemControls/Renderer.js
@@ -2,7 +2,6 @@ import { WebGLRenderer } from "three"
 
 export function createRenderer() {
     const renderer = new WebGLRenderer ({antialias: true})
-    // renderer.physicallyCorrectLights = true
 
     return renderer
 }
