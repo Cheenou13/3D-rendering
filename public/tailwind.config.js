68cb27c2e185b07babc31c32c7d2b70a5fa89d5f
Cheenou Xiong <cheenou.xiong@fewidev.com>
2022-05-05 01:25:06 -0500

    try
    Former-commit-id: ff7894a61e6efe7544440ed33a94a84910498af0

--- public/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['index.html', '/navigation-pages/global.html', '/navigation-pages/local-campus.html', 'navigation-pages/test-page.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
