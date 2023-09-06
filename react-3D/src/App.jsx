8c7f4ea99b579755ddaaa1077db90ec88b14f21b
chee <xiong726@uwm.edu>
2023-09-06 01:01:02 -0500

    add placeholder for landing page

--- react-3D/src/App.jsx
+++ react-3D/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import Landing from './components/landingPage/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className="read-the-docs text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Landing /> 
     </>
   )
 }
---
+++ react-3D/src/components/landingPage/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Landing() {
+
+  return (
+    <div className=' bg-slate-400 h-screen w-full'>
+        <div className='contents'>
+            <div className=' font-semibold'>Landing</div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
